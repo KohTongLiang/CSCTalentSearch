a58a4d62a5e3de5085f69e8fd3f2ec9d1aea2795
WQ <asdasd@asdasd.asdasd>
2016-07-28 14:04:33 +0800

    Fixed CDN, added some small stuff

--- TalentSearch/App_Start/BundleConfig.cs
+++ TalentSearch/App_Start/BundleConfig.cs
@@ -11,14 +11,13 @@ namespace TalentSearch
 
             bundles.UseCdn = true;   //enable CDN support
             BundleTable.EnableOptimizations = true;
-
+            
             bundles.Add(new ScriptBundle("~/bundles/jquery", "https://code.jquery.com/jquery-1.12.4.min.js"));
 
 
             bundles.Add(new ScriptBundle("~/bundles/modernizr", "https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap", "https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/site.css",
